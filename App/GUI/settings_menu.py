3ff25ae7e9c597fff45f78d01111dc1374e2e9a1
VillainousSsnake <cjdiamond9@gmail.com>
2026-03-15 12:35:07 -0400

    Added stub settings menu file

---
+++ App/GUI/settings_menu.py
@@ -0,0 +1,27 @@
+# /App/GUI/settings_menu.py
+# Contains settings menu code
+
+# Importing modules and libraries:
+import customtkinter as ctk
+
+
+# settings_menu function
+def settings_menu(app):
+
+    # Creating root window
+    root = ctk.CTk()
+    root.title("Configure Settings...  ")
+    root.geometry("850x525+200+200")
+
+    # Defining on_close function
+    def on_close():
+        root.destroy()
+        app.returnStatement = "exit"
+
+    # Assigning the buttons on the tkinter window top bar
+    root.protocol("WM_DELETE_WINDOW", on_close)
+
+    # TODO: Code goes here
+
+    # Root mainloop
+    root.mainloop()
