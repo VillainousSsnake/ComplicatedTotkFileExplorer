029ec1235ffabea40a13b428f0f4a11ac9a1dcaf
VillainousSsnake <cjdiamond9@gmail.com>
2026-03-15 12:29:32 -0400

    Added title menu

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -4,6 +4,7 @@
 
 # Importing modules and libraries:
 import customtkinter as ctk
+from CTkMenuBar import *
 
 
 # main_menu function
@@ -11,7 +12,7 @@ def main_menu(app):
 
     # Creating root window
     root = ctk.CTk()
-    root.title("HigharcheySDL Main Menu")
+    root.title("Main  ")
     root.geometry("850x525+200+200")
 
     # Defining on_close function
@@ -21,6 +22,18 @@ def main_menu(app):
 
     # Assigning the buttons on the tkinter window top bar
     root.protocol("WM_DELETE_WINDOW", on_close)
+
+    menu = CTkTitleMenu(root)
+    fileCascade = menu.add_cascade("File")
+    settingsCascade = menu.add_cascade("Settings")
+
+    fileDropdown = CustomDropdownMenu(widget=fileCascade)
+    fileDropdown.add_option(option="Open", command=lambda: print("Open"))
+    fileDropdown.add_option(option="Save", command=lambda: print("Save"))
+
+    dropdown3 = CustomDropdownMenu(widget=settingsCascade)
+    dropdown3.add_option(option="Configure", command=lambda: print("Configure"))
+    dropdown3.add_option(option="Update", command=lambda: print("Update"))
 
     # TODO: Code goes here
 
